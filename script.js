33577dc56dab4577265ffd2e72091c1447dfc39c
niallantony <88824327+niallantony@users.noreply.github.com>
2023-05-17 13:01:05 +0900

    Add a function to find a win

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const cell = (position) => {
-  let contents = "";
+const cell = (position,index) => {
+  let content = "";
   let cellDOM;
   const changeContents = (token) => {
-    contents = token;
+    gameboard.cells[index].content = token;
   };
   const drawCell = () => {
     const cellElement = document.createElement("div");
@@ -18,25 +18,52 @@ const cell = (position) => {
     changeContents(Player.token);
     console.log(cellDOM);
     cellDOM.innerText = Player.token;
+    gameboard.alleys.forEach((alley) => {
+      console.log(alley);
+      gameboard.checkAlleysForWin(alley);
+      console.log(alley.win);
+      if (alley.win) {console.log("That's a Bingo!")}
+    })
   }
 
-  return { position, contents, cellDOM, changeContents, drawCell, cellClick };
+  return { position, content, cellDOM, index, changeContents, drawCell, cellClick };
 };
 
 const gameboard = (() => {
   const cells = [
-    cell("top-left"),
-    cell("top-center"),
-    cell("top-right"),
-    cell("middle-left"),
-    cell("middle-center"),
-    cell("middle-right"),
-    cell("bottom-left"),
-    cell("bottom-center"),
-    cell("bottom-right"),
+    cell("top-left",0),
+    cell("top-center",1),
+    cell("top-right",2),
+    cell("middle-left",3),
+    cell("middle-center",4),
+    cell("middle-right",5),
+    cell("bottom-left",6),
+    cell("bottom-center",7),
+    cell("bottom-right",8),
   ];
 
-  return { cells };
+  const alley = (a,b,c) => {
+    let win = false;
+    return {win, a, b, c};
+  }
+
+  const alleys = [
+    alley(cells[0],cells[1],cells[2]),
+    alley(cells[3],cells[4],cells[5]),
+    alley(cells[6],cells[7],cells[8]),
+    alley(cells[0],cells[3],cells[6]),
+    alley(cells[1],cells[4],cells[7]),
+    alley(cells[2],cells[5],cells[8]),
+    alley(cells[0],cells[4],cells[8]),
+    alley(cells[2],cells[4],cells[6]),
+  ]
+
+  const checkAlleysForWin = (alley) => {
+    console.log(alley.a.content, alley.b.content, alley.c.content);
+    alley.win = alley.a.content === alley.b.content && alley.b.content === alley.c.content && alley.a.content !== '';
+  }
+
+  return { cells, alleys, checkAlleysForWin };
 })();
 
 const Player = (() => {
@@ -50,3 +77,4 @@ const displayController = (() => {
 })();
 
 gameboard.cells.forEach((cell) => cell.drawCell());
+console.log(gameboard.alleys);
